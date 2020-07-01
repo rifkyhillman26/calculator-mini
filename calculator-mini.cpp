54f8dc38ef6c4401977611e5e17280ce6a22c219
rifkyhillman26 <rifkyhilman265@gmail.com>
2020-07-01 13:02:19 -0700

    pertama kali belajar C++

---
+++ calculator-mini.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	int y;
+	int hasil;
+	
+	cout << "Masukkan angka: ";
+	cin >> x;
+	cout << "masukan angka lain: ";
+	cin >> y;
+	hasil = x + y;
+	cout << "hasil :" << hasil;
+	return 0;
+}
